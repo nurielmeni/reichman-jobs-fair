731717da28d0ae7516d6762a8fa5db44a4110879
Meni Nuriel <nurielmeni@gmail.com>
2022-04-18 11:07:24 +0300

    f

--- wp/wp-content/themes/niloos/footer.php
+++ wp/wp-content/themes/niloos/footer.php
@@ -11,43 +11,19 @@ include_once 'includes/SocialWalker.php';
 
 <?php do_action('reichman_content_after'); ?>
 
-<footer id="colophon" class="site-footer bg-primary p-4 sticky bottom-0" role="contentinfo">
+<footer id="colophon" class="site-footer bg-primary p-2 sticky bottom-0" role="contentinfo">
 	<?php do_action('reichman_footer'); ?>
 
 	<div class="container mx-auto flex flex-col justify-center items-center primary text-white">
 		<?php
-		wp_nav_menu(
-			array(
-				'container_id'    => 'footer-1',
-				'container_class' => 'mt-1 mb-2',
-				'menu_class'      => 'marker:text-green-400 list-disc pl-5 space-y-3 text-white',
-				'theme_location'  => 'footer-1',
-				'li_class'        => 'mx-4',
-				'walker' => new contact_description_walker,
-				'fallback_cb'     => false,
-			)
-		);
-
-		wp_nav_menu(
-			array(
-				'container_id'    => 'footer-2',
-				'container_class' => 'mt-1 mb-2',
-				'menu_class'      => 'marker:text-green-400 list-disc pl-5 space-y-3 text-white',
-				'theme_location'  => 'footer-2',
-				'li_class'        => 'mx-4',
-				'walker' => new contact_description_walker,
-				'fallback_cb'     => false,
-			)
-		);
-
 		// Social Menu
 		wp_nav_menu(
 			array(
 				'container_id'    => 'footer-social',
-				'container_class' => 'mt-1 mb-2',
+				'container_class' => '',
 				'menu_class'      => 'flex justify-center',
 				'theme_location'  => 'footer-social',
-				'li_class'        => 'px-2 mt-2',
+				'li_class'        => 'px-2 mt-2 mb-1',
 				'walker' => new social_walker,
 				'fallback_cb'     => false,
 			)
@@ -57,7 +33,7 @@ include_once 'includes/SocialWalker.php';
 
 	<div class="container mx-auto flex flex-col justify-center items-center text-white">
 		&copy; <?php echo date_i18n('Y'); ?> - <?= _e('All rights reserved to Reichman University.', 'reichman') ?>
-		<div class="text-sm">
+		<div class="text-xs">
 			POWERED BY <a href="https://niloosoft.com/he/">NILOOSOFT HUNTER EDGE</a> </div>
 	</div>
 
