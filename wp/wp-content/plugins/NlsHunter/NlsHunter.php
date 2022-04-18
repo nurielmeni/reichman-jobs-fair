ae781ad5f7f36073a6ecec7dfcaa922f4df49503
Meni Nuriel <nurielmeni@gmail.com>
2022-04-18 14:16:52 +0300

    d

--- wp/wp-content/plugins/NlsHunter/NlsHunter.php
+++ wp/wp-content/plugins/NlsHunter/NlsHunter.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       NlsHunter
  * Description:       API fot Niloosoft HunterHRMS System.
- * Version:           2.0.0
+ * Version:           2.0.22
  * Author:            Meni Nurie
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
